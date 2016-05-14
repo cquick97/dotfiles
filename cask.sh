fc1d37defafc5ad8a3a014a6f7de85b2d8943581
Deniz Basegmez <dbas12@gmail.com>
2016-05-13 20:11:39 -0400

    Add casks

---
+++ cask.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+brew cask install 1password
+brew cask install transmission
+
+# music
+brew cask install beardedspice
+brew cask install spotify
+
+# browsers
+brew cask install google-chrome
+brew cask install torbrowser
+
+# dev
+brew cask install virtualbox
+brew cask install iterm2
+
+# utilities
+brew cask install launchbar
+brew cask install hammerspoon
+brew cask install little-snitch
+brew cask install cheatsheet
