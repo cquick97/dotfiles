ab895213fee1e20941e8a744676c58eab49f5925
Connor Quick <connor@zer0w1re.net>
2016-05-16 01:22:39 -0400

    Added more applications to be installed

--- cask.sh
+++ cask.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# pwdmgr
 brew cask install 1password
+
+# torrent
 brew cask install transmission
 
-# music
+# media
 brew cask install beardedspice
 brew cask install spotify
+brew cask install vlc
 
 # browsers
 brew cask install google-chrome
@@ -13,6 +17,9 @@ brew cask install torbrowser
 
 # dev
 brew cask install virtualbox
+brew cask install vmware-fusion
+brew cask install eclipse-java
+brew cask install atom
 brew cask install iterm2
 
 # utilities
@@ -20,3 +27,26 @@ brew cask install launchbar
 brew cask install hammerspoon
 brew cask install little-snitch
 brew cask install cheatsheet
+brew cask install microsoft-word
+brew cask install bettertouchtool
+brew cask install cyberduck
+brew cask install gpgtools
+brew cask install logisim
+brew cask install smcfancontrol
+brew cask install the-unarchiver
+brew cask install teamviewer
+brew cask install tunnelblick
+brew cask install vnc-viewer
+
+# infosec
+brew cask install hopper-disassembler
+brew cask install maltego
+brew cask install wireshark
+
+# chat
+brew cask install skype
+brew cask install adium
+brew cask install teamspeak-client
+
+# games
+brew cask install steam
